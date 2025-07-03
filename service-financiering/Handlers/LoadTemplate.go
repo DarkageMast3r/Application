7433b96a7467050805f01822ad0c0cf3b5aa7550
David <DTploeg@gmail.com>
2025-07-03 11:01:10 +0200

    bug fix

--- service-financiering/Handlers/LoadTemplate.go
+++ service-financiering/Handlers/LoadTemplate.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadTemplate(wr http.ResponseWriter, templateName string, data any) error {
-	template, err := tmpl.New("Page").ParseFiles(templateName)
+	template, err := tmpl.New("Home").ParseFiles(templateName)
 	if err != nil {
 		fmt.Println("error rendering template")
 	}
