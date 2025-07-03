e7ebc18b290a2f580042026663b10abd9e29ca94
unknown <tijmen.peterson@solcon.nl>
2025-07-03 18:41:15 +0200

    Fix build

--- start.bat
+++ start.bat
@@ -1,21 +1,27 @@
 cd implementatie/cmd/server
 go build -o app.exe
 START /b app.exe
+
 cd ../../../service-discovery
 go build -o app.exe
 START /b app.exe
+
 cd ../service-financiering
 go build -o app.exe
 START /b app.exe
+
 cd ../service-signalering
 go build -o app.exe
 START /b app.exe
+
 cd ../technology-selection
 go build -o app.exe
 start /B app.exe
+
 cd ../ui
 go build -o app.exe
 start /B app.exe
-cd ../ZorgTechCatalogus_/cmd/server
+
+cd ../ZorgTechCatalogus/cmd/server
 go build -o app.exe
 START /b app.exe
